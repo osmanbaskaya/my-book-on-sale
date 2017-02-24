260d824ff06d7d872e7f8afee35e0a5771b575bc
osmanbaskaya <thyme.exe@gmail.com>
2017-02-24 15:30:16 -0800

    new version - book by book traverse

--- sale.py
+++ sale.py
@@ -1,12 +1,17 @@
 from __future__ import print_function
+import requests
 import mechanize
+import datetime
 import codecs
 from bs4 import BeautifulSoup
+from multiprocessing import Pool
 
 
 MAX_PAGE = 50
 AMAZON_KINDLE_STORE_URL = "https://www.amazon.com/b/ref=sv_kstore_4?ie=UTF8&node=11552285011"
 GOODREADS_URL = "https://www.goodreads.com/review/list/{}?shelf={}"
+GOODREADS_BASE_URL = "https://www.goodreads.com"
+HEADERS = {'User-agent': "Firefox"}
 
 
 def check_any_book_on_sale():
@@ -94,6 +99,92 @@ def get_books_from_goodreads(user_to_read_shelf_link, out_fn="books-in-to-read.t
     return goodreads_book_titles
 
 
+def get_amazon_price(amazon_book_response):
+    soup = BeautifulSoup(amazon_book_response.text, 'lxml')
+    prices = soup.find_all('input', {"name": "displayedPrice"})
+    if len(prices) != 0:
+        return prices[0]['value']
+    else:
+        prices = soup.find_all("span", {'class': "a-size-small a-color-price"})
+        if len(prices) != 0:
+            return prices[0].text[1:]
+        else:
+            prices = soup.find_all("span", {'class': "a-size-base mediaTab_subtitle"})
+            price_titles = soup.find_all("span", {'class': "a-size-large mediaTab_title"})
+            idx = -1
+            for i, t in enumerate(price_titles):
+                if t.text == 'eTextbook' or t.text == 'Kindle':
+                    idx = i
+                    break
+            if len(prices) != 0 and idx != -1:
+                return prices[0].text.strip()[1:]
+            else:
+                return "NA"
+
+
+def get_amazon_book_url(goodread_book_response):
+    soup = BeautifulSoup(goodread_book_response.text, 'lxml')
+    urls = soup.find_all('a', {"class": 'buttonBar'})
+    for url in urls:
+        if u'Amazon' in url.text:
+            return "{}{}".format(GOODREADS_BASE_URL, url['href'])
+
+
+def get_goodread_book_url(div):
+    return "{}{}".format(GOODREADS_BASE_URL, div.find('a')['href'])
+
+
+def get_book_price(item, headers=HEADERS):
+    book_url = get_goodread_book_url(item)
+    book_name = item.img['alt']
+    r = requests.get(book_url, headers=headers)
+    amazon_book_url = get_amazon_book_url(r)
+    amazon_book_response = requests.get(amazon_book_url, headers=headers)
+    price = get_amazon_price(amazon_book_response)
+    row = u"{}\t{}\t{}\t{}".format(book_url, amazon_book_url, book_name, price)
+    # print(u"{} {}".format(book_name, price))
+    return row
+
+
+def get_price_list_for_books(headers=HEADERS):
+
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--user', required=False, default="18882054-osman-ba-kaya",
+                        help="Provide user information formatted like 18882054-osman-ba-kaya")
+    parser.add_argument('--filename', required=False, default=None)
+    parser.add_argument('--shelf', required=False, default='to-read')
+    args = parser.parse_args()
+
+    today = datetime.datetime.today()\
+
+    print("Date: {}".format(today.strftime("%Y-%m-%d %H:%M")))
+
+    url = GOODREADS_URL.format(args.user, args.shelf)
+
+    user_to_read_shelf_url = "%s&view=covers&page={}" % url
+
+    items = []
+    for i in range(1, MAX_PAGE):
+        print("Goodreads page-%d processing" % i)
+        r = requests.get(user_to_read_shelf_url.format(i), headers=headers)
+        soup = BeautifulSoup(r.text, 'lxml')
+        curr_items = soup.find_all("div", {"class": "bookalike review"})
+        if len(curr_items) == 0:
+            break
+        items.extend(curr_items)
+
+    print("Total # of books: {}".format(len(items)))
+
+    # price_list = list(map(get_book_price, items))
+    pool = Pool(10)
+    price_list = pool.map(get_book_price, items)
+
+    out_fn = 'prices/{}-price-list.txt'.format(today.strftime("%Y-%m-%d"))
+    with codecs.open(out_fn, 'w', encoding='utf8') as f:
+        f.write("\n".join(price_list))
+
+
 if __name__ == '__main__':
-    check_any_book_on_sale()
+    get_price_list_for_books()
     print("Done.")
